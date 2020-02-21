5874d79885c2eaadf1eec35f5bc615bcbde07d18
ionut-banu <yonutbanu@gmail.com>
2020-02-21 23:39:51 +0200

    Adding battleship validator

---
+++ BattleshipValidator/src/com/ionut/BF.java
@@ -0,0 +1,13 @@
+package com.ionut;
+
+public class BF {
+
+    public BF(int[][] field) {
+        // your code here!
+    }
+
+    public boolean validate() {
+        // your code here too!
+        return false;
+    }
+}
